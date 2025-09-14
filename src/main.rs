e2a20a311f34b246acb61cda9e211d3c9bf1664e
aciddm3 <dmacid3@gmail.com>
2025-09-14 11:18:58 +0700

    Переработана структура исходников

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,11 @@
 use std::fs::File;
 use std::io::Read;
 use serde_json;
-use serde::Deserialize;
 
-type SectorID = u8;
-
-#[derive(Deserialize, Debug)]
-struct Sector {
-    id : SectorID,
-    relations : Vec<SectorID>,
-    is_based : bool,
-    cost : f64
-}
-
-fn total_cost (g : & Vec<Sector> ) -> f64 {
-    let mut s = 0.0;
-    for i in g {
-        if i.is_based {
-            s += i.cost
-        }
-    };
-    s
-}
-
-fn add_if_not_present<T: PartialEq>(vec: &mut Vec<T>, item: T) {
-    if !vec.contains(&item) {
-        vec.push(item);
-    }
-}
-
-fn generate_bool_combinations(n: usize) -> Vec<Vec<bool>> {
-    // Общее количество комбинаций равно 2 в степени n.
-    // Используем `2_u64.pow(n as u32)` для вычисления.
-    let num_combinations = 2_u64.pow(n as u32);
-    
-    let mut all_combinations = Vec::with_capacity(num_combinations as usize);
-    
-    // Итерируемся по каждому числу от 0 до 2^n - 1
-    for i in 0..num_combinations {
-        let mut current_combination = Vec::with_capacity(n);
-        let mut temp_i = i;
-        
-        // Преобразуем число `i` в двоичное представление
-        for _ in 0..n {
-            // Если остаток от деления на 2 равен 1, то это `true`, иначе `false`
-            current_combination.push(temp_i % 2 == 1);
-            temp_i /= 2; // Переходим к следующему биту
-        }
-        
-        // Нам нужен обратный порядок (младший бит в конце)
-        current_combination.reverse(); 
-        
-        all_combinations.push(current_combination);
-    }
-    
-    all_combinations
-}
-
+mod sector;
+mod utils;
+use sector::{Sector, SectorID};
+use utils::*;
 fn main() {
     let mut file = File::open("graph.json").expect("Не удалось открыть файл с графом");
     let mut data = String::new();
---
+++ src/sector.rs
@@ -0,0 +1,11 @@
+use serde::Deserialize;
+
+pub type SectorID = u8;
+
+#[derive(Deserialize, Debug)]
+pub struct Sector {
+    pub id : SectorID,
+    pub relations : Vec<SectorID>,
+    pub is_based : bool,
+    pub cost : f64
+}
